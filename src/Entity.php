2af76d28b0d0f083f734bb59d3e05ca1984fa063
Roman Nowicki <roman.nowicki@inbox.com>
2015-07-20 14:42:31 +0000

    modify Entity constructor to allow populating object by any traversable data type

--- src/Entity.php
+++ src/Entity.php
@@ -1,8 +1,18 @@
 <?php namespace Clean\Data;
 
+/**
+ * Entity object that extends stdClass constructor to allow passing traversable data to pupulate object properties
+ */
 class Entity
 {
-    final public function __construct(array $data = [ ])
+    /**
+     * __construct
+     *
+     * @param mixed $data Traversable data (array,stdClass,ArrayIterator)
+     *
+     * @return self
+     */
+    final public function __construct($data = [])
     {
         foreach ($data as $key => $item) {
             $this->$key = $item;
